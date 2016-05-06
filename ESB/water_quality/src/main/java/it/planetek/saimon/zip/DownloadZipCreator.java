e470f8c93ecad7f8f361d0489a6a2644a55e6e86
Gianluca Giorgio <giorgio@planetek.it>
2016-05-06 10:25:26 +0200

    Added zip download functionality

--- ESB/water_quality/src/main/java/it/planetek/saimon/zip/DownloadZipCreator.java
+++ ESB/water_quality/src/main/java/it/planetek/saimon/zip/DownloadZipCreator.java
@@ -3,6 +3,8 @@ package it.planetek.saimon.zip;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -11,55 +13,61 @@ import org.mule.api.MuleEventContext;
 import org.mule.api.lifecycle.Callable;
 import org.mule.api.transport.PropertyScope;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
 * 
 * @author M. Cosmai
 *
 */
-public class compressor  implements Callable 
-{	
-	
-	Properties wqProps;
-	
-	
-   //Constructor
-	public compressor(){
-		
-	
-		
-		
-	}
-	
+public class DownloadZipCreator  implements Callable 
+{
+	private static final Logger log = LoggerFactory.getLogger(DownloadZipCreator.class);
+
+	private Properties wqProps;
 	
 	/**
 	* Compressione prodotti SAIMON
 	* 
 	* @param SAIMON_Product		Path completo del file prodotto da compattare 
-	* @param SAIMON_Legenda		Path completo del file legenda da compattare 
 	* @return output 			URL http di download del file
+	* @throws IOException 
 	* 
 	*/
-	public String zip(String SAIMON_Product, String SAIMON_Legenda)
-	{ 
-    	
-		
+	public String zip(String SAIMON_Product) throws IOException
+	{
 		byte[] buffer = new byte[1024];
     	String MD_ZipFileName="";
     	String ZipFolder = 	wqProps.getProperty("download.zipfolder");
     	String WebFolder = wqProps.getProperty("download.webURL");
-    	String WebFullAddress = "";
     	
-    	String[] parts = SAIMON_Product.split("\\\\");
-    	String SAIMON_Product_fileName = parts[parts.length-1];
+    	String SAIMON_Product_fileName = Paths.get(SAIMON_Product).getFileName().toString();
+    	String SAIMON_Product_without_ext = SAIMON_Product_fileName.substring(0, SAIMON_Product_fileName.lastIndexOf('.'));
     	
-    	String[] parts1= SAIMON_Product_fileName.split("\\.");
-    	String SAIMON_Product_without_ext = parts1[0];
+    	
+    	
+    	
+    	
+    	
+    	
+    	String SAIMON_Product_type = SAIMON_Product_without_ext.split("_")[1];    	
+    	String SAIMON_Legenda = "/legenda/Legend_"+SAIMON_Product_type+".png";
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
     	
     	MD_ZipFileName = ZipFolder + SAIMON_Product_without_ext + ".zip"; 
     	
-    	try{
-    		System.out.println("Product file : " + SAIMON_Product);
-    		System.out.println("Legenda file : " + SAIMON_Legenda);
+    	try {
+    		log.info("Product file: " + SAIMON_Product);
+    		log.info("Legenda file: " + SAIMON_Legenda);
+    		log.info("Output zip file: " + MD_ZipFileName);
     		
     		FileOutputStream fos = new FileOutputStream(MD_ZipFileName);
     		ZipOutputStream zos = new ZipOutputStream(fos);
@@ -77,7 +85,7 @@ public class compressor  implements Callable
     		ZipEntry ze1= new ZipEntry("Legenda.png");
     		
     		zos.putNextEntry(ze1);
-    		FileInputStream in1 = new FileInputStream(SAIMON_Legenda);
+    		InputStream in1 = getClass().getResourceAsStream(SAIMON_Legenda);
     		
     		int len1;
     		while ((len1 = in1.read(buffer)) > 0) {
@@ -87,28 +95,24 @@ public class compressor  implements Callable
     		zos.closeEntry();
     		zos.close();
     		
-    	}catch(IOException ex){
+    	} catch(IOException ex) {
     	   ex.printStackTrace();
-    		
+    	   throw ex;
     	}
     	
-    	WebFullAddress = WebFolder + SAIMON_Product_without_ext + ".zip";
-    	return WebFullAddress;
+    	return WebFolder + SAIMON_Product_without_ext + ".zip";
 	}
 
 
 	@Override
-	public Object onCall(MuleEventContext eventContext) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public Object onCall(MuleEventContext eventContext) throws Exception
+	{
 		wqProps = eventContext.getMuleContext().getRegistry().get("wqProps");
 
-		String SAIMON_Product = eventContext.getMessage().getProperty("Path_Prodotto", PropertyScope.INVOCATION);
-		String SAIMON_Legenda = eventContext.getMessage().getProperty("Path_Lengenda", PropertyScope.INVOCATION);
+		String SAIMON_Product = eventContext.getMessage().getProperty("wqModisOutLocalFullPath", PropertyScope.INVOCATION);
 		
-		return zip(SAIMON_Product, SAIMON_Legenda);
+		return zip(SAIMON_Product);
 	}
-	
 	
 	
 }

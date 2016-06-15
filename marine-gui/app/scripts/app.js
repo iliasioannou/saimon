f363ce6be1fe4912732216578212fd1d1b8c7713
ferrulli <ferrulli@planetek.it>
2016-06-15 12:10:20 +0200

    fix dependencies 2

--- marine-gui/app/scripts/app.js
+++ marine-gui/app/scripts/app.js
@@ -20,7 +20,11 @@ angular
 		'ngRoute',
 		'ngSanitize',
 		'openlayers-directive',
+		'openlayers-layerswitcher',
 		'ui.bootstrap',
+		'angularAwesomeSlider',
+		'nvd3',
+		'smart-table',
     'services.config',
 		'flash',
 		'pascalprecht.translate'
